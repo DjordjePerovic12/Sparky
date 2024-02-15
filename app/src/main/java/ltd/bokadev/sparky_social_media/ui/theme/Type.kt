f0e1bbeab93f80eeca0d63c9c32a9e18e06b7dfa
Djordje Perovic <djordje.perovic@amplitudo.me>
2024-02-15 09:51:59 +0100

    Adds welcome screen UI, adds Primary button component, adds new color, type and string resources

--- app/src/main/java/ltd/bokadev/sparky_social_media/ui/theme/Type.kt
+++ app/src/main/java/ltd/bokadev/sparky_social_media/ui/theme/Type.kt
@@ -7,21 +7,33 @@ import javax.annotation.concurrent.Immutable
 
 @Immutable
 data class SparkyTypography(
-    val poppinsRegular20: TextStyle, val poppinsMedium32: TextStyle
+    val poppinsRegular14: TextStyle,
+    val poppinsRegular20: TextStyle,
+    val poppinsMedium16: TextStyle,
+    val poppinsMedium24: TextStyle,
+    val poppinsMedium32: TextStyle
 )
 
 val LocalSparkyTypography = staticCompositionLocalOf {
     SparkyTypography(
-        poppinsRegular20 = TextStyle.Default, poppinsMedium32 = TextStyle.Default
+        poppinsRegular14 = TextStyle.Default,
+        poppinsRegular20 = TextStyle.Default,
+        poppinsMedium16 = TextStyle.Default,
+        poppinsMedium24 = TextStyle.Default,
+        poppinsMedium32 = TextStyle.Default
     )
 }
 
 val sparkyTypography = SparkyTypography(
-    poppinsRegular20 = TextStyle(
+    poppinsRegular14 = TextStyle(
+        fontSize = 14.sp, lineHeight = 20.sp
+    ), poppinsRegular20 = TextStyle(
         fontSize = 20.sp
-    ),
-    poppinsMedium32 = TextStyle(
-        fontSize = 32.sp,
-        lineHeight = 40.sp
+    ), poppinsMedium16 = TextStyle(
+        fontSize = 16.sp, lineHeight = 24.sp
+    ), poppinsMedium24 = TextStyle(
+        fontSize = 24.sp, lineHeight = 32.sp
+    ), poppinsMedium32 = TextStyle(
+        fontSize = 32.sp, lineHeight = 40.sp
     )
 )
